26a62b53fc260f2b2321bc9e67cac25e81094a1e
ray.chang <yanghaochang1004@gmail.com>
2021-05-20 18:16:48 +0800

    feat(test_endpoint): add test_flaskr.py for testing

--- starter/backend/test_flaskr.py
+++ starter/backend/test_flaskr.py
@@ -1,6 +1,7 @@
 import os
 import unittest
 import json
+from flask.wrappers import Response
 from flask_sqlalchemy import SQLAlchemy
 
 from flaskr import create_app
@@ -19,13 +20,20 @@ class TriviaTestCase(unittest.TestCase):
             'localhost:5432', self.database_name)
         setup_db(self.app, self.database_path)
 
+        self.new_question = {
+            'question': 'this is a question',
+            'answer': 'this is an answer',
+            'category': '3',
+            'difficulty': '4',
+        }
+
         # binds the app to the current context
-        with self.app.app_context():
+        with self.app.app_context(): 
             self.db = SQLAlchemy()
             self.db.init_app(self.app)
             # create all tables
             self.db.create_all()
-    
+
     def tearDown(self):
         """Executed after reach test"""
         pass
@@ -34,6 +42,102 @@ class TriviaTestCase(unittest.TestCase):
     TODO
     Write at least one test for each test for successful operation and for expected errors.
     """
+    # test get categories
+
+    def test_get_categories(self):
+        response = self.client().get('/categories')
+        data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(data['success'])
+        self.assertTrue(data['categories'])
+
+    # test get questions
+    def test_get_questions(self):
+        response = self.client().get('/questions?page=1')
+        data = json.loads(response.data)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['questions'])
+        self.assertTrue(data['total_questions'])
+        self.assertTrue(data['categories'])
+
+    def test_404_get_questions(self):
+        response = self.client().get('/questions?page=100')
+        data = json.loads(response.data)
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'Not Found')
+
+    # test delete questions
+    def test_delete_questions(self):
+        response = self.client().delete('/questions/2')
+        data = json.loads(response.data)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertEqual(data['message'], 'Question Deleted')
+
+    def test_404_delete_questions(self):
+        response = self.client().delete('/questions/1000')
+        data = json.loads(response.data)
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'Not Found')
+
+    # test add a new question
+    def test_create_question(self):
+        response = self.client().post('/add/questions', json=self.new_question)
+        data = json.loads(response.data)
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(data['success'], True)
+        self.assertEqual(data['message'], 'Question Added')
+
+    def test_422_create_question(self):
+        response = self.client().post('/add/questions', json={
+            'question': 'this is a question',
+            'answer': 'this is an answer',
+            'category': 'Science',
+            'difficulty': '4',
+        })
+        data = json.loads(response.data)
+        self.assertEqual(response.status_code, 422)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'Unprocessable')
+
+    # search question by category
+    def test_search_question_by_category(self):
+        response = self.client().get('/categories/3/questions?page=1')
+        data = json.loads(response.data)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['questions'])
+        self.assertTrue(data['total_questions'])
+        self.assertTrue(data['current_category'])
+
+    def test_404_search_question_by_category(self):
+        response = self.client().get('/categories/10/questions?page=1')
+        data = json.loads(response.data)
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'Not Found')
+
+    # test get next question
+
+    def test_get_next_question(self):
+        response = self.client().post(
+            '/play/quizzes', json={"previous_question": [1, 2, 3, 4], "quiz_category": {"type": "History", "id": 3}})
+        data = json.loads(response.data)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['question'])
+
+    def test_422_get_next_question(self):
+        response = self.client().post(
+            '/play/quizzes', json={"previous_question": [2, 4, 5, 6, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33], "quiz_category": 3})
+        data = json.loads(response.data)
+        self.assertEqual(response.status_code, 422)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'Unprocessable')
 
 
 # Make the tests conveniently executable
